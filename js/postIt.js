107f73756e1d5f9dc4a71d9de73769e45b4aebed
Dominic Smith <dosmith@cern.ch>
2017-01-19 22:48:54 +0100

    mise a jour java script

--- js/postIt.js
+++ js/postIt.js
@@ -7,11 +7,22 @@ function alertWindow() {
 
 function openBox() {
     var message = prompt("Please provide a post-it", "Reply to emails!");
-
     if (message != null) {
 	document.getElementById("messages").innerHTML +=
 	    '<span class="note">'+message+'</span>';
+	validate(message);
     }
 }
 	
-   
+function validate(m) {
+    alert('Posted');
+}
+
+function keypress(event) {
+    var x = event.keyCode;     
+    var y = String.fromCharCode(x);
+    if (x == 112) {
+	openBox()
+    }
+}
+document.addEventListener("keydown", keypress, false);
